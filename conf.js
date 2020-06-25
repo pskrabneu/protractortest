0c2aa5341e81340505074bb3efb6e6e77221a3ac
Pavel Skrabneu <skrabneu@gmail.com>
2020-06-25 18:16:19 +0300

    Initial commit

---
+++ conf.js
@@ -0,0 +1,16 @@
+exports.config = {
+    directConnect: true,
+    framework: 'jasmine2',
+    seleniumAddress: 'http://localhost:4446/wd/hub',
+    specs: ['test/specs/*spec*.js'],
+    multiCapabilities: [{
+        // browserName: 'firefox'
+        browserName: 'chrome'
+    }, {
+        browserName: 'firefox'
+    }],
+    // options to be passed to Jasmine
+    jasmineNodeOpts: {
+        defaultTimeoutInterval: 30000
+    }
+};
