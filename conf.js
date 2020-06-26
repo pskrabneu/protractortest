394849e1d5e931fb2e1ae540e564588f152742f9
Pavel Skrabneu <skrabneu@gmail.com>
2020-06-26 19:38:01 +0300

    Calculator test, Addition

--- conf.js
+++ conf.js
@@ -2,11 +2,12 @@ exports.config = {
     directConnect: true,
     framework: 'jasmine2',
     seleniumAddress: 'http://localhost:4446/wd/hub',
+    // webdriver-manager start --seleniumPort 4446 (run with cli)
     specs: ['test/**/specs/*spec*.js'],
     multiCapabilities: [{
         browserName: 'chrome'
     }, {
-        browserName: 'firefox'
+        // browserName: 'firefox'
     }],
     // options to be passed to Jasmine
     jasmineNodeOpts: {
--- test/specs/calculator-test.spec.js
+++ test/specs/calculator-test.spec.js
@@ -1,13 +1,15 @@
 describe('Test calculator operations:', function () {
     var latest;
 
-    it('should add 2 + 18 with result 20', function () {
+    it('should open proper page', function () {
         browser.get('https://juliemr.github.io/protractor-demo/');
         const title = browser.getTitle();
         expect(title).toContain('Super Calculator');
+    });
 
+    it('should add 2 + 18 with result 20', function () {
         element(by.model('first')).sendKeys('2');
-        element(by.model('operator')).$(by.xpath('//option[@value="ADDITION"]')).click();
+        element(by.model('operator')).element(by.xpath('//option[@value="ADDITION"]')).click();
         element(by.model('second')).sendKeys('18');
         element(by.id('gobutton')).click();
 
